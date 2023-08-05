c857a396b29abca16eea215af47b15f7fb402eb5
Adrieli Alexandre Caetano <77703493+AdrieliCaetano@users.noreply.github.com>
2023-08-04 23:03:17 -0300

    Delete ProjetoBD/Receitas directory

--- ProjetoBD/Receitas/AppReceitas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Receita, Ingrediente, Ingredientes_receitas, Categoria
-
-admin.site.register(Receita)
-admin.site.register(Ingrediente)
-admin.site.register(Ingredientes_receitas)
-admin.site.register(Categoria)
