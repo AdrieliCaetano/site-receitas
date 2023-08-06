aef3f9f822dbee656d8b5f1f024f0eae54ab269c
Maria Eduarda <dudatimiraos@gmail.com>
2023-08-06 15:38:14 -0300

    Initial commit - front

---
+++ ProjetoBD/front/src/App.tsx
@@ -0,0 +1,17 @@
+import "./styles/main.css"
+
+function App() {
+
+  return (
+    <div clas>
+      <div className="bg-[#fc939a] w-screen h-screen">
+        teste
+      </div>
+      <div className="bg-white">
+        teste
+      </div>
+    </div>
+  )
+}
+
+export default App
