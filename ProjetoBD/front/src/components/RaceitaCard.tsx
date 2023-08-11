66af9867df217af33f34bc7e8b3745e2370e3288
Maria Eduarda <dudatimiraos@gmail.com>
2023-08-10 23:38:17 -0300

    Atualização do ReceitaCard

--- ProjetoBD/front/src/components/RaceitaCard.tsx
+++ ProjetoBD/front/src/components/RaceitaCard.tsx
@@ -1,6 +1,11 @@
+import {useState} from "react"
+import * as Dialog from "@radix-ui/react-dialog"
+import ReceitaInfo from "./ReceitaInfo"
+
 interface ReceitaCardProps {}
 
 function ReceitaCard(props: ReceitaCardProps) {
+  const [open, setOpen] = useState(false)
   return (
     <>
       <div className="bg-yellow-100 h-full rounded-md shadow-lg">
@@ -11,9 +16,13 @@ function ReceitaCard(props: ReceitaCardProps) {
         />
         <div className="flex flex-col text-center my-5 items-center gap-3">
           <p className="text-xl uppercase font-bold">Nome Da Receita</p>
-          <button className="bg-red-500 py-2 w-1/2 uppercase font-bold hover:bg-red-600 text-white rounded-md">
-            Ver Receita
-          </button>
+          <Dialog.Root open={open} onOpenChange={setOpen}>
+            <Dialog.Trigger className="bg-red-500 py-2 w-1/2 uppercase font-bold hover:bg-red-600 text-white rounded-md">
+              Ver Receita
+            </Dialog.Trigger>
+
+            <ReceitaInfo />
+          </Dialog.Root>
         </div>
       </div>
     </>
