b307692ba9d38c24a414fb56adb3c453720e6234
Maria Eduarda <dudatimiraos@gmail.com>
2023-08-10 23:37:55 -0300

    Adicionando o campo do usuário no modal de criação da receita

--- ProjetoBD/front/src/components/AddReceitaModal.tsx
+++ ProjetoBD/front/src/components/AddReceitaModal.tsx
@@ -12,7 +12,7 @@ function AddReceitaModal() {
     <>
       <Dialog.Portal>
         <Dialog.Overlay className="bg-black/50 inset-0 fixed" />
-        <Dialog.Content className="fixed bg-yellow-100 py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[70rem] h-[39rem] shadow-lg shadow-black/25">
+        <Dialog.Content className="fixed bg-yellow-100 py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[70rem] h-[38rem] shadow-lg shadow-black/25 overflow-auto">
           <Dialog.Title className="text-[#fc939a] uppercase font-bold text-center text-2xl font-serif p-2 border-[3px] border-[#fc939a] mb-10">
             Adicionar Nova Receita
           </Dialog.Title>
@@ -22,6 +22,27 @@ function AddReceitaModal() {
             className="relative grid grid-cols-2 gap-5"
           >
             <div>
+              <div className="flex flex-col gap-2">
+                <div className="flex flex-col mb-5">
+                  <label
+                    htmlFor=""
+                    className="font-bold text-[16px] text-[#fc939a] uppercase"
+                  >
+                    CHEF RESPONSÁVEL
+                  </label>
+                  <select
+                    {...register("chef", {required: true})}
+                    className="bg-gray-200 text-black rounded py-3 px-4 shadow-xl"
+                  >
+                    <option value=""></option>
+                    {/* {instituicoes.map((inst) => (
+                    <option key={inst["nome"]} value={inst["nome"]}>
+                      {inst["nome"]}
+                    </option>
+                  ))} */}
+                  </select>
+                </div>
+              </div>
               <div className="flex flex-col gap-2">
                 <div className="flex flex-col mb-5">
                   <label
@@ -141,7 +162,7 @@ function AddReceitaModal() {
               </div>
             </div>
 
-            <button className="bg-[#01141f] text-base font-bold rounded-md p-3 text-white hover:bg-[#01141f]/50 shadow-md shadow-black/25 absolute right-0">
+            <button className="bg-[#fc939a] text-base font-bold rounded-md p-3 text-white hover:bg-[#01141f]/50 shadow-md shadow-black/25 absolute right-0 bottom-4 uppercase">
               Adicionar
             </button>
           </form>
